10e659f5b1a97a1e5d8c8964ece25a7256bd581e
tj413316602 <413316602@qq.com>
2026-01-28 21:43:31 +0800

    build: 将Vite配置中的base路径从'/ReNamer/'改为相对路径'./'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 注意：如果部署到 https://<USERNAME>.github.io/<REPO>/，请取消注释并设置 base 为 '/<REPO>/'
-  base: '/ReNamer/',
+  base: './',
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
